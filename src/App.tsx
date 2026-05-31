6b1f8d809c57f53dd1fbaa6741e03bda1c0ea77b
rishabhsood123 <rishabhsood123@gmail.com>
2026-05-31 19:26:57 +0530

    Optimize scroll reveal threshold and rootMargin for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function HomePage() {
           }
         });
       },
-      { threshold: 0.15, rootMargin: '0px 0px -40px 0px' }
+      { threshold: 0.05, rootMargin: '0px 0px -20px 0px' }
     );
     els.forEach((el) => observer.observe(el));
     return () => observer.disconnect();
